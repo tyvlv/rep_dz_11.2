73bc27dfc882a1c6715b1a530c996b3bb892c4da
tychkov.vv <tyvl@yandex.ru>
2023-01-26 01:56:22 +0300

    add indication of returned variable type

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def upper_string(string):
+def upper_string(string) -> str:
     """Возвращает строку ЗАГЛАВНЫМИ буквами"""
     return string.upper()
 
 
-def title_string(string):
+def title_string(string) -> str:
     """Возвращает строку с Заглавной буквы"""
     return string.title()
